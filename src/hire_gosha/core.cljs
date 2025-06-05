aec5b265e870dfb9412548106dfc34e7cb803f81
Gosha Tcherednitchenko <git@gosha.net>
2025-06-05 17:23:59 +0100

    Full width projects

--- src/hire_gosha/core.cljs
+++ src/hire_gosha/core.cljs
@@ -151,11 +151,11 @@
 
 (defn projects-section [{:keys [projects]}]
   (let [sorted-projects (sort-by #(if (:important %) 0 1) projects)]
-    [:div.mx-auto.max-w-7xl.px-6.lg:px-8.py-16
-     [:div.mx-auto.max-w-2xl.text-center.mb-12
+    [:div.w-full.py-16
+     [:div.mx-auto.max-w-2xl.text-center.mb-12.px-6.lg:px-8
       [:h3.text-4xl.font-semibold.text-primary.mb-4 "Featured Projects"]
       [:p.mt-2.text-balance.text-primary "Some highlights from my recent work—"]]
-     [:div.grid.grid-cols-1.md:grid-cols-2.lg:grid-cols-3.gap-8
+     [:div.grid.grid-cols-1.md:grid-cols-2.lg:grid-cols-3.gap-8.px-6.lg:px-8
       (for [project sorted-projects]
         ^{:key (:title project)}
         (project-card-component project))]]))
