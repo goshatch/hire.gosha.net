99ea579df8f4e5f493bfc433d947d94992e92809
Gosha Tcherednitchenko <git@gosha.net>
2025-06-05 16:10:14 +0100

    Proper Tailwind v4 setup

---
+++ tailwind.config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  content: [
+    "./src/**/*.{clj,cljs,cljc}",
+    "./public/**/*.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': 'var(--color-text)',
+        'secondary': 'var(--color-text-subdued)',
+        'surface': 'var(--color-testimonials-bg)',
+      },
+      fontFamily: {
+        'sans': 'var(--font-sans)',
+        'mono': 'var(--font-mono)',
+      },
+      width: {
+        '120': '30rem',
+        '144': '36rem',
+      },
+      height: {
+        '80': '20rem',
+        '96': '24rem',
+      }
+    },
+  },
+  safelist: [
+    // Responsive classes for testimonials layout
+    'sm:grid-cols-2',
+    'lg:flex-row',
+    'lg:items-center', 
+    'lg:gap-12',
+    'lg:px-8',
+    'lg:w-144',
+    'lg:h-96',
+    'xl:mx-0',
+    'xl:max-w-none',
+    'xl:grid-cols-4',
+    'xl:contents',
+    'xl:space-y-0',
+    'xl:row-span-2',
+    'xl:row-start-1',
+    'xl:grid-flow-col',
+    // Color classes
+    'text-primary',
+    'text-secondary',
+    'bg-surface'
+  ],
+  plugins: [],
+}
