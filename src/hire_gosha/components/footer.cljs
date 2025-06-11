6f6dbe822023ac5b4f9ed3a3f18ac87466e06399
Gosha Tcherednitchenko <git@gosha.net>
2025-06-11 16:41:22 +0100

    Footer styling

--- src/hire_gosha/components/footer.cljs
+++ src/hire_gosha/components/footer.cljs
@@ -12,7 +12,7 @@
     [:div.border-t.border-gray-500.pt-8
      {:class ["flex" "flex-col" "md:flex-row" "md:justify-between" "gap-8"]}
      [:div
-      [:h3 {:class ["font-semibold" "mb-4"]} "I am unfortunately very online"]
+      [:h3.mb-4 "I am unfortunately very online:"]
       [:div {:class ["flex" "flex-col" "lg:flex-row" "gap-4"]}
        (social-link {:href "https://gosha.net" :emoji "🏴‍☠️" :label "Online Homepage"})
        (social-link {:href "https://linkedin.com/in/goshatch" :emoji "💼" :label "LinkedIn"})
@@ -21,7 +21,7 @@
        (social-link {:href "https://twitter.com/goshatch" :emoji "🐦" :label "Twitter"})
        (social-link {:href "https://bsky.app/profile/gosha.net" :emoji "🦋" :label "Bluesky"})]]
      [:div.flex-shrink-0
-      [:h3 {:class ["font-semibold" "mb-4"]} "About this page"]
+      [:h3.mb-4 "About this page"]
       [:p
        "Built with "
        [:a {:href "https://clojurescript.org"} "ClojureScript"]
